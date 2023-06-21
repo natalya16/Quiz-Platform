281cebca4c3dedf5c77a356bc5a67ebc3e64d380
natalya16 <137273166+natalya16@users.noreply.github.com>
2023-06-21 16:04:33 +0530

    Add files via upload

---
+++ quizquestions.js
@@ -0,0 +1,47 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does the abbreviation HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. How to make Lasagna",
+      "B. Hot Mail",
+      "C. Hyper Text Markup Language",
+      "D. How to Make Language",
+    ],
+  },
+  {
+    numb: 2,
+    question:
+      "What is the difference between an opening tag and a closing tag?",
+    answer: "B. Closing tag has a / in front",
+    options: [
+      "A. Opening tag has a / in front",
+       "B. Closing tag has a / in front",
+      "C. There is no difference",
+      "D. opening tag has ? in last",
+    ],
+  },
+  {
+    numb: 3,
+    question: " br  / > What type of tag is this?",
+    answer: "A. Break tag",
+    options: [
+      "A. Break tag",
+      "B. A broken one",
+      "C. An opening tag",
+      "D. An strong font Tag",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What is an element that does not have a closing tag called?",
+    answer: "B. Empty element",
+    options: [
+      "A. Tag",
+      "B. Empty element",
+      "C. Closed element",
+      "D. TExt Element",
+    ],
+  },
+];
